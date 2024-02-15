042f72bb1c48ed315f69ea6ab620894da3abc8e6
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-15 16:36:33 -0500

    work: Trabajando en templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
 
 # Vistas para Habitaciones
 def rooms(request):
-    return HttpResponse(request, 'hola')
+    return render(request, 'rooms.html')
