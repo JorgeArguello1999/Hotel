3d9ebd99e7cd486f3caa7e1335be60785432b1db
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-15 21:44:03 -0500

    feat: Modelos para las habitaciones creado

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import tipo
+from .models import estado
+from .models import habitaciones
+
 # Register your models here.
+admin.site.register(tipo)
+admin.site.register(estado)
+admin.site.register(habitaciones)
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# Estado de las habitaciones
+class estado(models.Model):
+    id = models.AutoField(primary_key=True)
+    estado = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f'{self.id} -> {self.estado}'
+
+# Tipo de habitaciones
+class tipo(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=20)
+    cama = models.IntegerField()
+    persona = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'ID: {self.id} Tipo: {self.tipo} Camas: {self.cama} Personas: {self.persona}'
+
+"""
+Esta tabla es la principal
+"""
+
+# Modelos para las habitaciones
+class habitaciones(models.Model):
+    id = models.AutoField(primary_key=True)
+    cuarto = models.IntegerField(default=100)
+    estado = models.ForeignKey(estado, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(tipo, on_delete=models.CASCADE)
+    observaciones = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.cuarto} -> {self.estado}'
