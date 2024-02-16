946dc3b2dcd5004b1a512447cfd8efe3420c0914
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-16 15:56:09 -0500

    feat: Funcion Delete para rooms creada

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.rooms, name='rooms')
+    path('', views.rooms, name='rooms'),
+    path('update/<int:id_habitacion>', views.rooms_update, name='rooms_update'),
+    path('delete/<int:id_habitacion>', views.rooms_delete, name='rooms_delete')
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
 # Formulario
 from rooms.forms import HabitacionForm
 # Modelo
@@ -17,12 +19,24 @@ def rooms(request):
         form = HabitacionForm(request.POST)
         if form.is_valid():
             form.save()
+        else:
+            print('Form NOT VALID')
     
-    if request.method == 'UPDATE':
-        # logica para actualizar los cuartos
-        pass
-
     return render(request, 'rooms.html', {
         'form': form,
         'lista': lista
     })
+
+def rooms_update(request, id_habitacion):
+    return redirect('rooms') 
+
+def rooms_delete(request, id_habitacion):
+    try:
+        # Buscar la habitación por su ID
+        habitacion = habitaciones.objects.get(pk=id_habitacion)
+        # Eliminar la habitación
+        habitacion.delete()
+    except Exception as e:
+        print(e)
+
+    return redirect('rooms')
