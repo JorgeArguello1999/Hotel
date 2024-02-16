f37388ac853546cf9cac3d10257c0cda2405aa3b
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-16 00:42:57 -0500

    feat: Modelo de reservaciones creado

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import estado
+from .models import reservacion
+
 # Register your models here.
+admin.site.register(estado)
+admin.site.register(reservacion)
