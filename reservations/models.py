23cbf8bc523befc5c1ad1b6f1796467b45646761
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-17 22:58:18 -0500

    feat: Validación para verificar qu la habitación elejida este desocupada

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Importamos las habitaciones
@@ -17,10 +18,26 @@ class reservacion(models.Model):
     habitacion = models.ForeignKey(habitaciones, on_delete=models.CASCADE)
     notas = models.TextField(default='...')
     tiempo_ingreso = models.DateField()
-    timepo_salida = models.DateField()
+    tiempo_salida = models.DateField()
     # Campo generado automaticamente
     fecha_agendamiento = models.DateField(auto_now=True)
     estado_reserva = models.ForeignKey(estado, on_delete=models.CASCADE)
 
+    def clean(self):
+        # Verificar que el tiempo de salida no sea menor al tiempo de ingreso
+        if self.tiempo_salida < self.tiempo_ingreso:
+            raise ValidationError("El tiempo de salida no puede ser menor al tiempo de ingreso.")
+
+        # Verificar si la habitación está ocupada para evitar agendar
+        if self.habitacion.estado.estado == 'Ocupada':
+            raise ValidationError("La habitación está ocupada y no se puede agendar.")
+
+    def save(self, *args, **kwargs):
+        # Verificar si la habitación está vacía para permitir agendar
+        if self.habitacion.estado.estado != 'Ocupado':
+            super().save(*args, **kwargs)
+        else:
+            raise ValidationError("La habitación no está vacía y no se puede agendar.")
+
     def __str__(self) -> str:
-        return f'{self.cedula} -> {self.habitacion} -> {self.estado_reserva}' 
+        return f'{self.cedula} -> {self.habitacion} -> {self.habitacion.estado.estado} -> {self.estado_reserva}' 
