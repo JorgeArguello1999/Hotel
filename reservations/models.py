dc0a3548b4c802cb3c33ac2886aa0de0358e50dd
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-18 00:38:58 -0500

    Estancado

--- reservations/models.py
+++ reservations/models.py
@@ -23,21 +23,5 @@ class reservacion(models.Model):
     fecha_agendamiento = models.DateField(auto_now=True)
     estado_reserva = models.ForeignKey(estado, on_delete=models.CASCADE)
 
-    def clean(self):
-        # Verificar que el tiempo de salida no sea menor al tiempo de ingreso
-        if self.tiempo_salida < self.tiempo_ingreso:
-            raise ValidationError("El tiempo de salida no puede ser menor al tiempo de ingreso.")
-
-        # Verificar si la habitación está ocupada para evitar agendar
-        if self.habitacion.estado.estado == 'Ocupada':
-            raise ValidationError("La habitación está ocupada y no se puede agendar.")
-
-    def save(self, *args, **kwargs):
-        # Verificar si la habitación está vacía para permitir agendar
-        if self.habitacion.estado.estado != 'Ocupado':
-            super().save(*args, **kwargs)
-        else:
-            raise ValidationError("La habitación no está vacía y no se puede agendar.")
-
     def __str__(self) -> str:
         return f'{self.cedula} -> {self.habitacion} -> {self.habitacion.estado.estado} -> {self.estado_reserva}' 
--- reservations/serializer.py
+++ reservations/serializer.py
@@ -4,14 +4,17 @@ from rest_framework import serializers
 from reservations.models import estado
 from reservations.models import reservacion
 
+# Serializadores Externos
+from rooms.serializer import Habitaciones_Serializer
+
 class Estados_Serializer(serializers.ModelSerializer):
     class Meta:
         model = estado
         fields = '__all__'
 
 class Reservaciones_Serializer(serializers.ModelSerializer):
-    # Anidando información
     estado_reserva = Estados_Serializer()
+    habitacion = Habitaciones_Serializer()  # Anidando información de la habitación
 
     class Meta:
         model = reservacion
