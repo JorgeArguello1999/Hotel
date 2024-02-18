9e5abaae37048990c988235b84d69bd76fba4f6a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-18 02:16:26 -0500

    feat: Modelos creados

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# Modelos
+from rooms.models import TypesModel
+from rooms.models import StatusModel
+from rooms.models import RoomsModel
+
+# Tipos de Habitación
+admin.site.register(TypesModel)
+admin.site.register(StatusModel)
+admin.site.register(RoomsModel)
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Tipos de Habitaciones
+class TypesModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    bed = models.IntegerField(default=0)
+    person = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'{self.name} -> Camas:{self.bed} -> Personas:{self.person}'
+
+# Estado de las Habitaciones
+class StatusModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+# Modelo de Habitaciones 
+class RoomsModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    number = models.IntegerField()
+    status = models.ForeignKey(StatusModel, on_delete=models.CASCADE)
+    types = models.ForeignKey(TypesModel, on_delete=models.CASCADE)
+    notes = models.TextField(default='', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.number} -> {self.status.name} -> {self.types.name} {self.types.bed} {self.types.person} -> {self.notes}'
