c8301f02997a7400dcd15427a85463677b884475
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-19 15:31:05 -0500

    fix: Change the env variable

--- main/settings.py
+++ main/settings.py
@@ -12,7 +12,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
