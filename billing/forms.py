7ca2ce9d651d8db0f4097b6cfca292a8d033aac4
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-20 15:15:16 -0500

    feat: Modificando campos del formulario

--- billing/forms.py
+++ billing/forms.py
@@ -5,6 +5,11 @@ from billing.models import BillingModel
 
 # Formulario
 class BillingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Configura el widget del campo 'date' para que sea un campo de fecha
+        self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
+       
     class Meta:
         model = BillingModel
         fields = ['client', 'date', 'iva', 'discounts', 'value']
