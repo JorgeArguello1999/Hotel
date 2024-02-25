57a781ed80374463c447aeae31ba64ecf098a232
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-02-25 13:45:14 -0500

    work: Ya tenemos los primero filtros faltan estilos

--- rooms/views.py
+++ rooms/views.py
@@ -18,22 +18,33 @@ from rooms.forms import RoomForm
 ## Rooms 
 @login_required
 def rooms(request):
-    # Metodo GET
-    if request.method == 'GET':
-        form = RoomForm()
-        items = RoomsModel.objects.select_related('status', 'types').all().order_by('number')
-
-        return render(request, 'rooms.html', {
-            'form': form,
-            'items': items
-        })
-
     # Verificamos si es POST y si el formulario esta correcto
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('rooms')
+
+    # Cargamos los filtros
+    status_filter = request.GET.get('status')
+    types_filter = request.GET.get('type')
+
+    items = RoomsModel.objects.select_related('status', 'types').all().order_by('number')
+    status = StatusModel.objects.all()
+    types = TypesModel.objects.all()
+
+    if status_filter: items = items.filter(status__name__icontains=status_filter)
+    if types_filter: items = items.filter(types__id=types_filter)
+
+    # Formulario
+    form = RoomForm()
+
+    return render(request, 'rooms.html', {
+        'form': form,
+        'items': items,
+        'types': types,
+        'status': status
+    })
 
 @login_required
 def rooms_delete(request, id_room:int):
